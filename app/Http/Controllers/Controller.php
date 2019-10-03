4e1c169dd1985eb64cde915c8368a1533a51a138
Martin Kun <martin.kun@parall.ax>
2019-10-03 12:31:40 +0100

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\View\View;
+use Illuminate\Support\Facades\View;
 
 class Controller extends BaseController
 {
