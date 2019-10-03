b6253cda3b03d0fb18a2a76d5c39244ffea3a51d
Martin Kun <martin.kun@parall.ax>
2019-10-03 12:39:25 +0100

    update

--- resources/views/programs/byteminer.blade.php
+++ resources/views/programs/byteminer.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@php
+    $player = \App\Player::where('user_id', \Illuminate\Support\Facades\Auth::id())->first();
+@endphp
+
 @section('content')
     <div class="container">
         <div class="row">
