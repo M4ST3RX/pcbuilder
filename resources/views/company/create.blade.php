90caa82225c3e095482ee9370ca80144149d4556
Martin Kun <martin.kun@parall.ax>
2019-10-03 15:53:45 +0100

    update

--- resources/views/company/create.blade.php
+++ resources/views/company/create.blade.php
@@ -8,6 +8,9 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
+                @if(Session::has('message'))
+                    <p class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
+                @endif
                 <div class="card">
                     <div class="card-header">{{ __('Create Company') }}</div>
 
