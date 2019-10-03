1681103a9ddcfa654844933f26b8d9fd98f77848
Martin Kun <martin.kun@parall.ax>
2019-10-03 17:46:54 +0100

    update

--- app/Player.php
+++ app/Player.php
@@ -8,6 +8,6 @@ class Player extends Model
 {
     public function computers()
     {
-        return $this->hasMany(Computer::class, 'id', 'user_id');
+        return $this->hasMany(Computer::class, 'user_id', 'user_id');
     }
 }
